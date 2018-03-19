5b541f5d17f96b2ae7f532b17f6451bb16be9b96
Kevin Somers-Higgins <kev.higgins@gmail.com>
2018-03-18 22:25:54 -0500

    Add sad air dash

--- Assets/CharacterController2D/demo/scripts/DemoScene.cs
+++ Assets/CharacterController2D/demo/scripts/DemoScene.cs
@@ -15,6 +15,10 @@ public class DemoScene : MonoBehaviour
 	public float inAirDamping = 5f;
 	public float jumpHeight = 3f;
 
+    public float dashDuration = .75f;
+    public float dashSpeed = 15f;
+    public float dashCooldown = 1f;
+
 	[HideInInspector]
 	private float normalizedHorizontalSpeed = 0;
 
@@ -23,6 +27,17 @@ public class DemoScene : MonoBehaviour
 	private RaycastHit2D _lastControllerColliderHit;
 	private Vector3 _velocity;
 
+    private bool _lastFrameWasAirDash = false;
+    private bool _isDashing = false;
+    private float? _dashTimeAccumulation;
+    private bool _hasAirDashedSinceJump = false;
+    private float? _timeSinceDashStart;
+    
+
+    //private bool _wasAirborne = false;
+
+    
+    
 
 	void Awake ()
 	{
@@ -71,8 +86,11 @@ public class DemoScene : MonoBehaviour
 	// the Update loop contains a very simple example of moving the character around and controlling the animation
 	void Update ()
 	{
-		if (_controller.isGrounded)
-			_velocity.y = 0;
+	    if (_controller.isGrounded)
+	    {
+	        _velocity.y = 0;
+	        _hasAirDashedSinceJump = false;
+	    }
 
 		if (Input.GetAxisRaw ("Horizontal") > 0f) {
 			normalizedHorizontalSpeed = 1;
@@ -95,20 +113,62 @@ public class DemoScene : MonoBehaviour
 				_animator.Play (Animator.StringToHash ("Idle"));
 		}
 
-
 		// we can only jump whilst grounded
 		if (_controller.isGrounded && Input.GetButtonDown ("Jump")) {
 			_velocity.y = Mathf.Sqrt (2f * jumpHeight * -gravity);
 			_animator.Play (Animator.StringToHash ("Jump"));
 		}
 
+	    if (_dashTimeAccumulation.HasValue)
+	    {
+	        _dashTimeAccumulation += Time.deltaTime;
+	        if (_dashTimeAccumulation >= dashDuration)
+	        {
+	            _isDashing = false;
+	        }
+	    }
 
-		// apply horizontal speed smoothing it. dont really do this with Lerp. Use SmoothDamp or something that provides more control
-		var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
-		_velocity.x = Mathf.Lerp (_velocity.x, normalizedHorizontalSpeed * runSpeed, Time.deltaTime * smoothedMovementFactor);
+	    if (_timeSinceDashStart.HasValue)
+	    {
+	        _timeSinceDashStart += Time.deltaTime;
+	        if (_timeSinceDashStart >= dashCooldown)
+	        {
+	            _timeSinceDashStart = null;
+	        }
+	    }
 
-		// apply gravity before moving
-		_velocity.y += gravity * Time.deltaTime;
+        if (Input.GetButtonDown("Air Dash") 
+            && _lastFrameWasAirDash == false 
+            //&& _hasAirDashedSinceJump == false
+            && !_timeSinceDashStart.HasValue)
+	    {
+	        _lastFrameWasAirDash = true;
+	        _hasAirDashedSinceJump = true;
+	        _isDashing = true;
+	        _timeSinceDashStart = 0f;
+            
+	        _dashTimeAccumulation = 0f;
+	    }
+	    else
+	    {
+	        _lastFrameWasAirDash = false;
+	    }
+
+	    if (_isDashing)
+	    {
+	        var directionMultiplier = transform.localScale.x > 0 ? 1 : -1;
+            _velocity.x += directionMultiplier * dashSpeed;
+	    }
+	    else
+	    {
+	        // apply horizontal speed smoothing it. dont really do this with Lerp. Use SmoothDamp or something that provides more control
+	        var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
+	        _velocity.x = Mathf.Lerp(_velocity.x, normalizedHorizontalSpeed * runSpeed, Time.deltaTime * smoothedMovementFactor);
+	        // apply gravity before moving
+	        _velocity.y += gravity * Time.deltaTime;
+        }
+
+
 
 //		 if holding down bump up our movement amount and turn off one way platform detection for a frame.
 //		 this lets us jump down through one way platforms
@@ -121,7 +181,9 @@ public class DemoScene : MonoBehaviour
 			_velocity.y = airBreakSpeed;
 		}
 
-		_controller.move( _velocity * Time.deltaTime );
+
+
+        _controller.move( _velocity * Time.deltaTime );
 
 		// grab our current _velocity to use as a base for all calculations
 		_velocity = _controller.velocity;

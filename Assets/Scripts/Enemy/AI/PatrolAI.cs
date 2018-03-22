0329c3a60b044d06c0f99eb2facd83a06343cdb5
Kevin Somers-Higgins <kev.higgins@gmail.com>
2018-03-21 23:29:31 -0500

    Added PatrolAI script

--- Assets/Scripts/Enemy/AI/PatrolAI.cs
+++ Assets/Scripts/Enemy/AI/PatrolAI.cs
@@ -1,12 +1,81 @@
-﻿using UnityEngine;
+﻿using System;
+using Assets.Scripts.Abilities;
+using Prime31;
+using UnityEngine;
 
 namespace Assets.Scripts.Enemy.AI
 {
-    public class PatrolAI : MonoBehaviour
+    [RequireComponent(typeof(HorizontalMoveAbility), typeof(CharacterController2D))]
+    public class PatrolAI : MonoBehaviour, IUnitVelocity
     {
+        enum Direction
+        {
+            Left,
+            Right
+        }
+
+        public float VelocityX { get { return _velocity.x; } }
+        public float VelocityY { get { return _velocity.y; } }
+
+        public float gravity = -25f;
+
+        private HorizontalMoveAbility _horizontalMoveAbility;
+        private CharacterController2D _controller;
+
+        private Direction _direction = Direction.Left;
+        private Vector3 _velocity;
+        private float _lastDistance;
+
+        void Awake()
+        {
+            _horizontalMoveAbility = GetComponent<HorizontalMoveAbility>();
+            _controller = GetComponent<CharacterController2D>();
+        }
+
         void Update()
         {
-            // Move forward
+            if (_controller.isGrounded)
+            {
+                _velocity.y = 0;
+            }
+
+            _velocity.y += gravity * Time.deltaTime;
+
+            if (_controller.isGrounded)
+            {
+                if (_direction == Direction.Left)
+                {
+                    _horizontalMoveAbility.TryLeft();
+                }
+                else
+                {
+                    _horizontalMoveAbility.TryRight();
+                }
+
+                _velocity.x = _horizontalMoveAbility.Speed;
+
+                var moveVector = _velocity * Time.deltaTime;
+                var predictedPosition = transform.position + moveVector;
+                var origin = new Vector2(predictedPosition.x, predictedPosition.y);
+
+                var result = Physics2D.Raycast(origin, Vector2.down, 1, _controller.platformMask);
+
+                if (result.collider == null)
+                {
+                    SwitchDirection();
+                    _velocity.x = 0;
+                }
+                _lastDistance = result.distance;
+            }
+
+            _controller.move(_velocity * Time.deltaTime);
+
+            
+        }
+
+        private void SwitchDirection()
+        {
+            _direction = _direction == Direction.Left ? Direction.Right : Direction.Left;
         }
     }
 }

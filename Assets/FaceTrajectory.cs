8ce0b6ea4d699f8cd34b0de340788e7deef08df4
Nagoda <jeowulf55@gmail.com>
2018-04-02 23:09:25 -0500

    TURRET IMPROVEMENTS

---
+++ Assets/FaceTrajectory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceTrajectory : MonoBehaviour {
+
+public Transform obj;
+[SerializeField]
+private Vector3 previousLocation;
+	[SerializeField]
+private Vector3 currentTrajectory;
+
+	// Use this for initialization
+	void Start () {
+		obj = transform.root;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (obj.transform.position != previousLocation) 
+		{
+			currentTrajectory = obj.transform.position - previousLocation;
+			transform.right = currentTrajectory;
+			previousLocation = obj.transform.position;
+		}
+
+		
+	}
+}
